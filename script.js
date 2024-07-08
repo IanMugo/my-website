90e4e550af69ae5609623b2885bada9b2eb5636f
IanMugo <Ianduncan240@gmail.com>
2024-07-08 09:59:42 +0300

    my website

---
+++ script.js
@@ -0,0 +1,48 @@
+// declaring variables
+x = 10;
+
+var y = 15;
+
+let corse = "web development"
+
+// variable name = variable value
+
+//arithmetic operators
+
+const pi = 3.1452;
+
+console.log(x+y);
+
+console.log(x-y);
+
+console.log(x*y);
+
+console.log(x/y);
+
+console.log(x%y); //modulus
+
+console.log(++x); //increment --x decrement
+
+// greater>,less<,equal ==,equal value and equal data type ===, <=, >=
+
+console.log(x>y); // etc
+
+console.log(x<=y);
+
+//array
+let fruits = ["apple","oranges","banana"]
+console.log(fruits);
+
+//objects
+let person = {name:"ian", course:"web", org:"plp"}
+
+//undefined data type...has no value e.g.
+let subject;
+console.log(subject);
+
+//function
+
+function addition(a, b){
+    return a + b;
+}
+console.log(addition(100, 200));
